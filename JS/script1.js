c09c5a359cdf26939dc77cf7ee139a1c03b66c83
iharisrao <raomharis07@gmail.com>
2026-01-04 10:42:27 +0500

    uploading of restaurant image and script.js

---
+++ JS/script1.js
@@ -0,0 +1,14 @@
+// --- 1. DATA: The Restaurant Memory ---
+const restaurants = [
+    {
+        name: "McDonald's",
+        image: "../images/mcdonalds.jpg",
+        description: "Quick, reliable fast food with burgers and fries – perfect for late-night cravings.",
+        vibes: ["Quick Snack", "Family Dinner", "Late Night"],
+        walk: "4 min walk",
+        rating: 4.3,
+        reviews: 312,
+        openTime: 0, 
+        closeTime: 24
+    }
+]

6b8ccce9345a67e909eafcab6e75adb436a6496c
marwahayat023-lab <marwahayat023@gmail.com>
2026-01-04 10:47:41 +0500

    Adding image of kfc

--- JS/script1.js
+++ JS/script1.js
@@ -10,5 +10,16 @@ const restaurants = [
         reviews: 312,
         openTime: 0, 
         closeTime: 24
-    }
+    },
+    {
+        name: "KFC",
+        image: "../images/kfc.jpg",
+        description: "Crispy fried chicken and zinger burgers – the ultimate comfort food spot.",
+        vibes: ["Quick Snack", "Family Dinner", "Late Night"],
+        walk: "5 min walk",
+        rating: 4.2,
+        reviews: 289,
+        openTime: 11,
+        closeTime: 2
+    },
 ]
